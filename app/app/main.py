00931b4a656330aafb7baddb0b4fa8ac353491e8
richard <richard@realizesec.com>
2021-07-21 01:53:59 +0100

    branch working

--- app/app/main.py
+++ app/app/main.py
@@ -3,10 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.mount("/static", StaticFiles(directory="/vol/static"), name="static")
 templates = Jinja2Templates(directory="templates/")
+
 
 @app.get("/", response_class=HTMLResponse)
 @app.get("/home", response_class=HTMLResponse)
--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+/bin/cp -r /app/static/* /vol/static
 uvicorn main:app --host=0.0.0.0 --port=5000
