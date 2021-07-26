9fff2988b7d436ba6b7ff01ec9b37e63dafc83b4
richard <richard@realizesec.com>
2021-07-26 22:31:34 +0100

    SSL certs setup

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+sed -i "s/FULLCHAINREPLACE/$SSL_FULLCHAIN/g" /etc/nginx/conf.d/default.conf
+sed -i "s/PRIVKEYREPLACE/$SSL_PRIVKEY/g"     /etc/nginx/conf.d/default.conf
+
 nginx -g 'daemon off;'
