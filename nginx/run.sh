f9a13264607440758822cdcc3b83370a1e2633b6
Richard <richard@realizesec.com>
2021-12-26 04:21:35 -0800

    created certs volume

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
 set -e
-
-sed -i "s/FULLCHAINREPLACE/$SSL_FULLCHAIN/g" /etc/nginx/conf.d/default.conf
-sed -i "s/PRIVKEYREPLACE/$SSL_PRIVKEY/g"     /etc/nginx/conf.d/default.conf
-
 nginx -g 'daemon off;'
